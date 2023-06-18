07dd231100f72f383f7924ae2aea5b9738e02d22
Joachim <danmayr.joachim@fronius.com>
2023-06-18 19:03:45 +0200

    chore: fix seg predict

--- test-seg.sh
+++ test-seg.sh
@@ -8,4 +8,4 @@ export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 cd /workspaces/open-bio-image-trainer
 
 
-python /yolov5/predict.py --source `pwd`/projects/$projectname/test/images/ --weights `pwd`/projects/$projectname/result2/weights/best.pt --conf 0.25 --name $projectname --project `pwd`/projects/$projectname/testrun --conf-thres 0.04 --iou-thres 0.04 --view-img --save-conf
+python /yolov5/segment/predict.py --source `pwd`/projects/$projectname/test/images/ --weights `pwd`/projects/$projectname/result5/weights/best.pt --conf 0.25 --name $projectname --project `pwd`/projects/$projectname/testrun --conf-thres 0.04 --iou-thres 0.04 --view-img --save-conf
